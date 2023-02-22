f0195e12b54a3de91c76742c1181af6b5a7e746b
Arhizmei <arhizmei@gmail.com>
2023-02-22 11:49:32 +0200

    password

--- Constant/app/src/main/java/com/example/constant/MainActivity.kt
+++ Constant/app/src/main/java/com/example/constant/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.constant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.constant.constans.Constans
 import com.example.constant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,20 +17,45 @@ class MainActivity : AppCompatActivity() {
 
         bindingClass.button1.setOnClickListener {
 
-            val name=bindingClass.editText1.text.toString()
+            val name=bindingClass.editName.text.toString()
             when(name){
-            "andrey" -> {
+
+            Constans.DIRECTOR -> {
+
                 bindingClass.textView1.visibility = View.VISIBLE
-                bindingClass.textView1.text = "получите 1000 грн"
+                val tempText = "получите ${Constans.DIRECTOR_SALARY}"
+
+                bindingClass.textView1.text = if (bindingClass.password.text.toString() == Constans.DIRECTOR_PASSWORD){
+
+                    tempText}
+                else {
+                     "неверный пароль"
+                }
             }
-                "sergey" -> {
+                Constans.INGENER -> {
                     bindingClass.textView1.visibility = View.VISIBLE
-                    bindingClass.textView1.text = "получите 1500 грн"
+                    val tempText = "получите ${Constans.INGENER_SALARY}"
+                    bindingClass.textView1.text = if (bindingClass.password.text.toString() == Constans.INGINER_PASSWORD){
+
+                        tempText}
+                    else {
+                        "неверный пароль"
+                    }
                 }
 
-                "evgeniy" -> {
+                Constans.DVORNIK -> {
+
                     bindingClass.textView1.visibility = View.VISIBLE
-                    bindingClass.textView1.text = "получите 2500 грн"
+
+                    val tempText = "получите ${Constans.DVORNIK_SALARY}"
+
+                    bindingClass.textView1.text = if (bindingClass.password.text.toString() == Constans.DVORNIK_PASSWORD){
+
+                        tempText}
+                    else {
+                        "неверный пароль"
+                    }
+
                 }
                 else -> {
                     bindingClass.textView1.visibility = View.VISIBLE
@@ -40,4 +66,7 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+
     }
+
---
+++ Constant/app/src/main/java/com/example/constant/constans/Constans.kt
@@ -0,0 +1,15 @@
+package com.example.constant.constans
+
+object Constans {
+    const val DIRECTOR_SALARY = 1000
+    const val INGENER_SALARY = 1500
+    const val DVORNIK_SALARY = 2500
+
+    const val DIRECTOR = "andrey"
+    const val INGENER = "sergey"
+    const val DVORNIK = "vadim"
+
+    const val DIRECTOR_PASSWORD = "1234"
+    const val INGINER_PASSWORD = "12345"
+    const val DVORNIK_PASSWORD = "123456"
+}
