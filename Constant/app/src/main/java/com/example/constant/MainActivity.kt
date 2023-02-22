3fa12ccec435d6bf0a5b88d81419816bfe7dafd7
Arhizmei <arhizmei@gmail.com>
2023-02-22 10:29:37 +0200

    first salary

--- Constant/app/src/main/java/com/example/constant/MainActivity.kt
+++ Constant/app/src/main/java/com/example/constant/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.constant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.constant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.button1.setOnClickListener {
+
+            val name=bindingClass.editText1.text.toString()
+            when(name){
+            "andrey" -> {
+                bindingClass.textView1.visibility = View.VISIBLE
+                bindingClass.textView1.text = "получите 1000 грн"
+            }
+                "sergey" -> {
+                    bindingClass.textView1.visibility = View.VISIBLE
+                    bindingClass.textView1.text = "получите 1500 грн"
+                }
+
+                "evgeniy" -> {
+                    bindingClass.textView1.visibility = View.VISIBLE
+                    bindingClass.textView1.text = "получите 2500 грн"
+                }
+                else -> {
+                    bindingClass.textView1.visibility = View.VISIBLE
+                    bindingClass.textView1.text = "нет такого работника"
+
+                }
+            }
+
+            }
+        }
     }
-}
